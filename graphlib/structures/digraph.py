3b0dd13fe2ce370c2981629dac840cb95d86de12
Anatoly.s <tolik.frnk@yandex.ru>
2022-04-13 13:20:22 +0300

    improve init + add subgraph, random selection functions to the Graph-class

--- graphlib/structures/digraph.py
+++ graphlib/structures/digraph.py
@@ -1,20 +1,37 @@
 '''Cтруктура данных для неориентированного невзвешенного графа'''
+import random
 
 
 class Graph:
-    def __init__(self, name='null', nodes=None):
+    def __init__(self, name='null', nodes=None, edges=None):
         self._name = name
+
         if nodes is None:
             self._nodes = set()
         else:
             self._nodes = set(nodes)
-        # список смежности для каждой вершины;
-        self._edges = dict([(node, []) for node in self._nodes])
+
+        if edges is not None:
+            if type(edges) is dict:
+                self._edges = edges
+            elif type(edges) is list:
+                # список смежности для каждой вершины;
+                self._edges = dict([(node, []) for node in self._nodes])
+                for (u, v) in edges:
+                    self.add_edge(u, v)
+        else:
+            # список смежности для каждой вершины;
+            self._edges = dict([(node, []) for node in self._nodes])
 
     @property
     def nodes(self):
         # вершины графа
         return self._nodes
+
+    @property
+    def edges(self):
+        # список смежности для каждой вершины графа
+        return self._edges
 
     @property
     def nodes_count(self):
@@ -29,8 +46,14 @@ class Graph:
 
     def add_node(self, u):
         # добавление вершины
-        self._nodes.add(u)
-        self._edges[u] = []
+        if u not in self._nodes:
+            self._nodes.add(u)
+            self._edges[u] = []
+
+    def add_nodes(self, list_with_nodes):
+        # добавление вершины
+        for u in list_with_nodes:
+            self.add_node(u)
 
     def add_edge(self, u, v):
         # добавление ребра
@@ -49,4 +72,25 @@ class Graph:
         result = f'Graph <{self._name}> with {self.nodes_count} nodes and {self.edges_count} edges'
         return result
 
-    #def subgraph(self):
+    def random_selection(self, x):
+        # случайный выбор 'x' вершин из графа
+        new_nodes = random.sample(self.nodes, k=x)
+        return new_nodes
+
+    def subgraph(self, nodes=None, x=3):
+        # подграф исходного графа
+
+        if nodes is None:
+            # вершины выбираются случайным образом
+            sub_nodes = set(self.random_selection(x))
+        else:
+            # с определенными вершинами
+            sub_nodes = set(nodes)
+
+        sub_edges = dict([(node, [adj_node for adj_node in self._edges[node] if adj_node in sub_nodes])
+                          for node in sub_nodes])
+
+        subgraph = Graph(name='subgraph::' + self._name,
+                         nodes=sub_nodes,
+                         edges=sub_edges)
+        return subgraph
