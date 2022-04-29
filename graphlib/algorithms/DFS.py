f6217d5aad331436556752d0f905342eae5c5652
Anatoly.s <tolik.frnk@yandex.ru>
2022-04-29 08:42:29 +0300

    add recursive version of DFS with time marks

--- graphlib/algorithms/DFS.py
+++ graphlib/algorithms/DFS.py
@@ -42,3 +42,28 @@ def DFS_with_cc(graph, largest=False):
         max_component_number = max(cc, key=lambda num: len(cc[num]))
         return cc_num, max_component_number, cc
     return cc_num, cc
+
+
+def DFS_with_times_recursive(graph):
+    # рекурсивный DFS с временными метками
+    time_in = dict()     # время входа в вершину
+    time_out = dict()    # время выхода из вершины
+    time = 1
+    visited = set()
+
+    def explore(node):
+        nonlocal time  # для изменения локальной переменной функции на уровень выше
+        visited.add(node)
+        time_in[node] = time
+        time += 1
+
+        for adj_node in graph.neighbors_for_node(node):
+            if adj_node not in visited:
+                explore(adj_node)
+        time_out[node] = time
+        time += 1
+
+    for u in graph.nodes:
+        if u not in visited:
+            explore(u)
+    return time_in, time_out
